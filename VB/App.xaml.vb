889be7fa9a229280966440a5255b0f9f068590a7
DevExpressExampleBot <fakeEmail>
2022-12-16 20:03:28 +0400

    Source auto update [skip ci]

--- VB/App.xaml.vb
+++ VB/App.xaml.vb
@@ -1,14 +1,11 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Configuration
-Imports System.Linq
 Imports System.Windows
 
 Namespace CustomResources
+
     ''' <summary>
     ''' Interaction logic for App.xaml
     ''' </summary>
-    Partial Public Class App
+    Public Partial Class App
         Inherits Application
 
     End Class
